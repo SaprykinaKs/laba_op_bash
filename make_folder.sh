3f1c28e217a6e2227d29a61774b782cce427c20c
Ксения Сапрыкина <kseniasaprykina@MacBook-Air-Ksenia.local>
2024-10-07 13:34:16 +0300

    fix make_folder

--- make_folder.sh
+++ make_folder.sh
@@ -2,9 +2,15 @@
 # bash make_folder.sh
 
 IMAGE_SIZE=1g
-IMAGE_PATH="/Users/kseniasaprykina/Downloads/op/op/disk_image.dmg"
+IMAGE_NAME="disk_image.dmg"  
+WORKING_DIR="$(pwd)"  
+
+mkdir -p "$WORKING_DIR"
+
+IMAGE_PATH="$WORKING_DIR/$IMAGE_NAME"
+
 VOLUME_NAME="MyDisk"
-MOUNT_POINT="/Users/kseniasaprykina/Downloads/op/op/MyDisk"
+MOUNT_POINT="$WORKING_DIR/MyDisk" 
 
 hdiutil create -size "$IMAGE_SIZE" -fs HFS+ -volname "$VOLUME_NAME" "$IMAGE_PATH"
 
